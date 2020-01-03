740a13e14e9e1938d4cbed6234b12c05ed676a55
zhuqiqi <zhuqiqi@topdraw.cn>
2020-01-03 14:42:05 +0800

    单例模式一些辅助注释。

--- src/main/java/com/zor/advanced/designPattern/singleton/EagerSingleton.java
+++ src/main/java/com/zor/advanced/designPattern/singleton/EagerSingleton.java
@@ -2,17 +2,16 @@ package com.zor.advanced.designPattern.singleton;
 
 /**
  * 简单饿汉式
- *缺点： 一上来就创建对象了，如果该实例从始至终都没被使用过，则会造成内存浪费；
+ * 缺点： 一上来就创建对象了，如果该实例从始至终都没被使用过，则会造成内存浪费；
  * 但是是线程安全的，因为初始化静态数据时，Java提供了的线程安全性保证。(所以不需要任何的同步)
- *
  */
 public class EagerSingleton {
+    // 2.在类的内部创建自行实例
+    private static EagerSingleton eagerSingleton = new EagerSingleton();
+
     // 1.将构造函数私有化，不可以通过new的方式来创建对象
     private EagerSingleton() {
     }
-
-    // 2.在类的内部创建自行实例
-    private static EagerSingleton eagerSingleton = new EagerSingleton();
 
     // 3.提供获取唯一实例的方法
     public static EagerSingleton getInstance() {
@@ -20,4 +19,3 @@ public class EagerSingleton {
     }
 
 }
-
--- src/main/java/com/zor/advanced/designPattern/singleton/LazySingleton.java
+++ src/main/java/com/zor/advanced/designPattern/singleton/LazySingleton.java
@@ -2,7 +2,7 @@ package com.zor.advanced.designPattern.singleton;
 
 
 /**
- * 双重检测机制(DCL)懒汉式
+ * 双重检测机制(DCL)懒汉式 double-checked locking双重检测加锁
  * 参考
  * https://mp.weixin.qq.com/s?__biz=MzI4Njg5MDA5NA==&mid=2247484239&idx=1&sn=6560be96e456b513cb1e4f78a740a258&chksm=ebd7424edca0cb584906fb97679cf2ca557f430fbc87d2c86ce0652d2e3c36c2528466942df5&scene=21###wechat_redirect
  */
@@ -15,7 +15,7 @@ public class LazySingleton {
     private static LazySingleton getInstance() {
         if (lazySingleton == null) {//判空为了提升性能
             synchronized (LazySingleton.class) {
-                if (lazySingleton == null) {//进入区域后，再检查一次，如果仍是null,才创建实例
+                if (lazySingleton == null) {//进入区域后，再检查一次，如果仍是null,才创建实例，这里如果不判空可能会实例化多次
                     lazySingleton = new LazySingleton();
                 }
             }
