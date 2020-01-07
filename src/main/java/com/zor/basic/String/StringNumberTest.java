c0fdc6da348e9760210ded9b4157d235086d6216
zhuqiqi <zhuqiqi@topdraw.cn>
2020-01-07 14:39:03 +0800

    字符串测试。

--- src/main/java/com/zor/basic/String/StringNumberTest.java
+++ src/main/java/com/zor/basic/String/StringNumberTest.java
@@ -8,7 +8,26 @@ import org.junit.Test;
 public class StringNumberTest {
 
     @Test
-    public void test() {
+    public void StringTest1() {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = "he" + "llo";//编译期间做了优化，没有生成新对象
+        String s4 = "hel" + new String("lo");
+        String s5 = new String("hello");
+        String s6 = s5.intern();
+        String s7 = "h";
+        String s8 = "ello";
+        String s9 = s7 + s8;
+        System.out.println(s1 == s2);//true
+        System.out.println(s1 == s3);//true
+        System.out.println(s1 == s4);//false
+        System.out.println(s1 == s9);//false
+        System.out.println(s4 == s5);//false
+        System.out.println(s1 == s6);//true
+    }
+
+    @Test
+    public void StringTest2() {
         Integer i1 = 100, i2 = 100, i3 = 200, i4 = 200;
 
         System.out.println(i1 == i2);//true IntegerCache -128~127
@@ -38,6 +57,18 @@ public class StringNumberTest {
         String s4 = s3 + s2;
         System.out.println(s1 == s4);//true
 
+    }
+    //TODO ??????
+    @Test
+    public void StringTest3() {
+        String s1 = new String("hello");
+        String intern1 = s1.intern();
+        String s2 = "hello";
+        System.out.println(s1 == s2);//false
+        String s3 = new String("hello") + new String("hello");
+        String intern3 = s3.intern();
+        String s4 = "hellohello";
+        System.out.println(s3 == s4);//为啥是true
     }
 
     @Test
