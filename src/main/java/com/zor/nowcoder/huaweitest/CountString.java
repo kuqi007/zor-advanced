ef3690af70d1d841b04ab87a168b20d245df7dd6
zhuqiqi <zhuqiqi@topdraw.cn>
2020-02-01 19:41:41 +0800

    牛客华为机试题。

---
+++ src/main/java/com/zor/nowcoder/huaweitest/CountString.java
@@ -0,0 +1,43 @@
+package com.zor.nowcoder.huaweitest;
+
+import java.util.Scanner;
+
+/**
+ * Created by zqq on 2020/2/1.
+ * 写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+ * <p>
+ * 输入描述:
+ * 第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
+ * <p>
+ * 输出描述:
+ * 输出输入字符串中含有该字符的个数。
+ * <p>
+ * 示例1
+ * 输入
+ * 复制
+ * ABCDEF
+ * A
+ * 输出
+ * 复制
+ * 1
+ */
+public class CountString {
+    public static void main(String[] args) {
+        solution1();
+
+    }
+
+    private static void solution1() {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().toLowerCase();
+        char c = scanner.nextLine().charAt(0);
+        c = Character.toLowerCase(c);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/com/zor/nowcoder/huaweitest/LastWordLength.java
@@ -0,0 +1,50 @@
+package com.zor.nowcoder.huaweitest;
+
+import java.util.Scanner;
+
+/**
+ * Created by zqq on 2020/2/1.
+ * 题目描述
+ * 计算字符串最后一个单词的长度，单词以空格隔开。
+ * 输入描述:
+ * 一行字符串，非空，长度小于5000。
+ * <p>
+ * 输出描述:
+ * 整数N，最后一个单词的长度。
+ * <p>
+ * 示例1
+ * 输入
+ * hello world
+ * 输出
+ * 5
+ */
+public class LastWordLength {
+
+    public static void main(String[] args) {
+        solution1();
+        //solution2();
+
+    }
+
+
+    private static void solution1() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] s = str.split("\\s+");
+        System.out.println(s[s.length - 1].length());
+    }
+
+    private static void solution2() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == ' ') {
+                break;
+            }
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
