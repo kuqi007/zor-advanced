87a5d71a2813f7f76050247d129f1ad1e783a029
zhuqiqi <zhuqiqi@topdraw.cn>
2020-02-05 20:16:47 +0800

    solution3

--- src/main/java/com/zor/nowcoder/huaweitest/ExtractUniqueNum.java
+++ src/main/java/com/zor/nowcoder/huaweitest/ExtractUniqueNum.java
@@ -8,13 +8,13 @@ import java.util.Set;
  * Created by zqq on 2020/2/5.
  * 题目描述
  * 输入一个int型整数，按照从右向左的阅读顺序，返回一个不含重复数字的新的整数。
- *
+ * <p>
  * 输入描述:
  * 输入一个int型整数
- *
+ * <p>
  * 输出描述:
  * 按照从右向左的阅读顺序，返回一个不含重复数字的新的整数
- *
+ * <p>
  * 示例1
  * 输入
  * 9876673
@@ -24,7 +24,31 @@ import java.util.Set;
 public class ExtractUniqueNum {
     public static void main(String[] args) {
         //solution1();
-        solution2();
+        //solution2();
+        solution3();
+    }
+
+    private static void solution3() {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        String str;
+        int count = 0;
+        char ch;
+        char star = '*';
+
+        while (sc.hasNext()) {
+            n = sc.nextInt();
+            str = Integer.toString(n);
+
+            while (count < str.length()) {
+                ch = str.charAt(str.length() - 1 - count);
+                if (ch != star)
+                    System.out.print(ch);
+                str = str.replace(ch, star);
+                count++;
+            }
+
+        }
     }
 
     private static void solution1() {
