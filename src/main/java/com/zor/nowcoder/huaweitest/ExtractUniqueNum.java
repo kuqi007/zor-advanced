280c124d983a601164102df7263fe73e28dddb5a
zhuqiqi <zhuqiqi@topdraw.cn>
2020-02-05 17:15:13 +0800

    华为机试提交。

---
+++ src/main/java/com/zor/nowcoder/huaweitest/ExtractUniqueNum.java
@@ -0,0 +1,64 @@
+package com.zor.nowcoder.huaweitest;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * Created by zqq on 2020/2/5.
+ * 题目描述
+ * 输入一个int型整数，按照从右向左的阅读顺序，返回一个不含重复数字的新的整数。
+ *
+ * 输入描述:
+ * 输入一个int型整数
+ *
+ * 输出描述:
+ * 按照从右向左的阅读顺序，返回一个不含重复数字的新的整数
+ *
+ * 示例1
+ * 输入
+ * 9876673
+ * 输出
+ * 37689
+ */
+public class ExtractUniqueNum {
+    public static void main(String[] args) {
+        //solution1();
+        solution2();
+    }
+
+    private static void solution1() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int input = scanner.nextInt();
+            StringBuilder s = new StringBuilder();
+            while (input != 0) {
+                int i = input % 10;
+                if (s.indexOf(String.valueOf(i)) == -1) {
+                    s.append(i);
+                }
+                input = input / 10;
+            }
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * 使用LinkedHashSet
+     */
+    private static void solution2() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            Set<Character> integerSet = new LinkedHashSet<>(str.length());
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = str.length() - 1; i >= 0; i--) {
+                integerSet.add(str.charAt(i));
+            }
+            for (Character character : integerSet) {
+                stringBuilder.append(character);
+            }
+            System.out.println(stringBuilder);
+        }
+    }
+}
---
+++ src/main/java/com/zor/nowcoder/huaweitest/MergeSameRecord.java
@@ -0,0 +1,58 @@
+package com.zor.nowcoder.huaweitest;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * Created by zqq on 2020/2/5.
+ * 数据表记录包含表索引和数值（int范围的整数），请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照key值升序进行输出。
+ * <p>
+ * 输入描述:
+ * 先输入键值对的个数
+ * 然后输入成对的index和value值，以空格隔开
+ * <p>
+ * 输出描述:
+ * 输出合并后的键值对（多行）
+ * <p>
+ * 示例1
+ * 输入
+ * 4
+ * 0 1
+ * 0 2
+ * 1 2
+ * 3 4
+ * 输出
+ * 0 3
+ * 1 2
+ * 3 4
+ */
+public class MergeSameRecord {
+    public static void main(String[] args) {
+        solution1();
+    }
+
+    private static void solution1() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int count = scanner.nextInt();
+            Map<Integer, Integer> treeMap = new TreeMap<>();
+            for (int i = 0; i < count; i++) {
+                int key = scanner.nextInt();
+                int value = scanner.nextInt();
+                if (treeMap.containsKey(key)) {
+                    treeMap.put(key, treeMap.get(key) + value);
+                } else {
+                    treeMap.put(key, value);
+                }
+            }
+            for (int key : treeMap.keySet()) {
+                System.out.println(key + " " + treeMap.get(key));
+            }
+            //treeMap.forEach((key, value) -> {
+            //    System.out.println(key + " " + value);
+            //});
+        }
+
+    }
+}
