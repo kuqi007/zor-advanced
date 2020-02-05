0b22167615f34ef5904ba7735c696ffcddf6dc5b
zhuqiqi <zhuqiqi@topdraw.cn>
2020-02-05 15:30:30 +0800

    注释提交。

--- src/main/java/com/zor/nowcoder/huaweitest/MathRound.java
+++ src/main/java/com/zor/nowcoder/huaweitest/MathRound.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 /**
  * Created by zqq on 2020/2/5.
+ * 写出一个程序，接受一个正浮点数值，输出该数值的近似整数值。如果小数点后数值大于等于5,向上取整；小于5，则向下取整。
+ *
+ * 输入描述:
+ * 输入一个正浮点数值
+ *
+ * 输出描述:
+ * 输出该数值的近似整数值
+ *
+ * 示例1
+ * 输入
+ * 5.5
+ * 输出
+ * 6
  */
 public class MathRound {
     public static void main(String[] args) {
--- src/main/java/com/zor/nowcoder/huaweitest/PrintPrimeNum.java
+++ src/main/java/com/zor/nowcoder/huaweitest/PrintPrimeNum.java
@@ -4,6 +4,32 @@ import java.util.Scanner;
 
 /**
  * Created by zqq on 2020/2/3.
+ * 功能:输入一个正整数，按照从小到大的顺序输出它的所有质数的因子（如180的质数因子为2 2 3 3 5 ）
+ *
+ * 最后一个数后面也要有空格
+ *
+ * 详细描述：
+ *
+ * 函数接口说明：
+ *
+ * public String getResult(long ulDataInput)
+ *
+ * 输入参数：
+ * long ulDataInput：输入的正整数
+ * 返回值：
+ * String
+ *
+ * 输入描述:
+ * 输入一个long型整数
+ *
+ * 输出描述:
+ * 按照从小到大的顺序输出它的所有质数的因子，以空格隔开。最后一个数后面也要有空格。
+ *
+ * 示例1
+ * 输入
+ * 180
+ * 输出
+ * 2 2 3 3 5
  */
 public class PrintPrimeNum {
     public static void main(String[] args) {
