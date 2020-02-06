e3c21db4b7d821804919fdc6c5017b068bfddd7f
zhuqiqi <zhuqiqi@topdraw.cn>
2020-02-06 21:15:28 +0800

    华为机试提交。

---
+++ src/main/java/com/zor/nowcoder/huaweitest/Dec2Bin.java
@@ -0,0 +1,41 @@
+package com.zor.nowcoder.huaweitest;
+
+import java.util.Scanner;
+
+/**
+ * Created by zqq on 2020/2/6.
+ */
+public class Dec2Bin {
+    public static void main(String[] args) {
+        solution1();
+        //solution2();
+    }
+
+
+    private static void solution1() {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        int count = 0;
+        while (i != 0) {
+            int b = i % 2;
+            if (b == 1) {
+                count++;
+            }
+            i = i / 2;
+        }
+        System.out.println(count);
+    }
+
+    private static void solution2() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) > 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/com/zor/nowcoder/huaweitest/SortWords.java
@@ -0,0 +1,54 @@
+package com.zor.nowcoder.huaweitest;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by zqq on 2020/2/6.
+ * 给定n个字符串，请对n个字符串按照字典序排列。
+ * 输入描述:
+ * 输入第一行为一个正整数n(1≤n≤1000),下面n行为n个字符串(字符串长度≤100),字符串中只含有大小写字母。
+ * 输出描述:
+ * 数据输出n行，输出结果为按照字典序排列的字符串。
+ * 示例1
+ * 输入
+ * 9
+ * cap
+ * to
+ * cat
+ * card
+ * two
+ * too
+ * up
+ * boat
+ * boot
+ * 输出
+ * boat
+ * boot
+ * cap
+ * card
+ * cat
+ * to
+ * too
+ * two
+ * up
+ */
+public class SortWords {
+    public static void main(String[] args) {
+        solution1();
+    }
+
+    private static void solution1() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            String str = scanner.next();
+            strings[i] = str;
+        }
+        Arrays.sort(strings);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
