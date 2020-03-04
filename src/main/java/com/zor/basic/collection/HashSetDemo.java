a4c5ae5e339a97dd30ee9ed3e2762edbd6aad775
Sonder <kuqi007@qq.com>
2020-03-04 23:18:00 +0800

    HashSet Demo

---
+++ src/main/java/com/zor/basic/collection/HashSetDemo.java
@@ -0,0 +1,23 @@
+package com.zor.basic.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://www.cnblogs.com/ideal-20/p/11146733.html
+ */
+public class HashSetDemo {
+    public static void main(String[] args) {
+
+        //下列程序执行结果是否有序
+        Set<Integer> hs = new HashSet<Integer>();
+        for (int i = 30; i >= 0; i--) {
+            hs.add(i);
+        }
+
+        //增强for遍历
+        for (Integer s : hs) {
+            System.out.print(s + " ");
+        }
+    }
+}
