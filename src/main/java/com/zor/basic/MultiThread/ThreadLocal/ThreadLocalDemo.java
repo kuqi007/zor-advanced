bd4e62f893a8eed104a986c6ce1989ca3fe9f0f7
zhuqiqi <zhuqiqi@topdraw.cn>
2020-03-05 18:05:27 +0800

    test

--- src/main/java/com/zor/basic/MultiThread/ThreadLocal/ThreadLocalDemo.java
+++ src/main/java/com/zor/basic/MultiThread/ThreadLocal/ThreadLocalDemo.java
@@ -4,4 +4,7 @@ package com.zor.basic.MultiThread.ThreadLocal;
  * Created by zqq on 2020/3/2.
  */
 public class ThreadLocalDemo {
+    public static void main(String[] args) {
+        ThreadLocal<String> localVar=new InheritableThreadLocal<>();
+    }
 }
