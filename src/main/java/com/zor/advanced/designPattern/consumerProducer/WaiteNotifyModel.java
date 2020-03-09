7fb8f97ffd071cb4ee292d2bc9d7f433a28c45b0
Sonder <kuqi007@qq.com>
2020-03-09 20:58:14 +0800

    生产者消费者模式。

---
+++ src/main/java/com/zor/advanced/designPattern/consumerProducer/WaiteNotifyModel.java
@@ -0,0 +1,81 @@
+package com.zor.advanced.designPattern.consumerProducer;
+
+/**
+ * https://my.oschina.net/hosee/blog/485121
+ */
+public class WaiteNotifyModel {
+    private static Integer count = 0;
+    private final Integer FULL = 10;
+    private static String LOCK = "LOCK";
+
+    class Producer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == FULL) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    count++;
+                    System.out.println(Thread.currentThread().getName()
+                            + "生产者生产，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+
+    }
+
+    class Consumer implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == 0) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                            e.printStackTrace();
+                        }
+                    }
+                    count--;
+                    System.out.println(Thread.currentThread().getName()
+                            + "消费者消费，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        WaiteNotifyModel hosee = new WaiteNotifyModel();
+        new Thread(hosee.new Producer()).start();
+        new Thread(hosee.new Consumer()).start();
+        new Thread(hosee.new Producer()).start();
+        new Thread(hosee.new Consumer()).start();
+
+        new Thread(hosee.new Producer()).start();
+        new Thread(hosee.new Consumer()).start();
+        new Thread(hosee.new Producer()).start();
+        new Thread(hosee.new Consumer()).start();
+    }
+
+}
