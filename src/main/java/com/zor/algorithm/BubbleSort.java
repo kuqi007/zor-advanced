79fa6a1105464d4b91ec409b1ffcac210212625c
zhuqiqi <zhuqiqi@topdraw.cn>
2020-03-18 14:41:01 +0800

    冒泡排序。

--- src/main/java/com/zor/algorithm/BubbleSort.java
+++ src/main/java/com/zor/algorithm/BubbleSort.java
@@ -1,4 +1,25 @@
 package com.zor.algorithm;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arrays = new int[]{9, 3, 10, 1, 8};
+        int temp;
+        boolean isChange = true;
+        for (int i = 0; i < arrays.length - 1 && isChange; i++) {
+            isChange = false;
+            for (int j = 0; j < arrays.length - i - 1; j++) {
+                if (arrays[j] > arrays[j + 1]) {
+                    temp = arrays[j];
+                    arrays[j] = arrays[j + 1];
+                    arrays[j + 1] = temp;
+                    isChange = true;
+                    System.out.println(Arrays.toString(arrays));
+                }
+            }
+        }
+    }
+
 }
