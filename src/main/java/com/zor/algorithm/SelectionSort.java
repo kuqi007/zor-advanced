5d95e5aee2958504c536213a91bb91b950cf8b88
zhuqiqi <zhuqiqi@topdraw.cn>
2020-03-18 16:06:45 +0800

    选择排序

---
+++ src/main/java/com/zor/algorithm/SelectionSort.java
@@ -0,0 +1,26 @@
+package com.zor.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Created by zqq on 2020/3/18.
+ */
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arrays = new int[]{9, 3, 10, 1, 8};
+        int pos;
+        int temp;
+        for (int i = 0; i < arrays.length - 1; i++) {
+            pos = 0;
+            for (int j = 0; j < arrays.length - i; j++) {
+                if (arrays[j] > arrays[pos]) {
+                    pos = j;
+                }
+            }
+            temp = arrays[pos];
+            arrays[pos] = arrays[arrays.length - 1 - i];
+            arrays[arrays.length - 1 - i] = temp;
+            System.out.println(Arrays.toString(arrays));
+        }
+    }
+}
