640e38053f900ed78f64b310c430d608ef9f82c3
Sonder <kuqi007@qq.com>
2020-03-23 17:37:21 +0800

    冒泡排序的另一种写法。

--- src/main/java/com/zor/algorithm/sort/BubbleSort.java
+++ src/main/java/com/zor/algorithm/sort/BubbleSort.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arrays = new int[]{9, 3, 10, 1, 8};
-        sort(arrays);
+        int[] arrays = new int[]{9, 8, 7, 6, 5};
+        //sort1(arrays);
+        sort2(arrays);
     }
 
-    private static void sort(int[] arrays) {
+    private static void sort1(int[] arrays) {
         int temp;
         boolean isChange = true;
         // 外层循环是排序的趟数，需要n-1趟排序
@@ -27,6 +28,30 @@ public class BubbleSort {
                 }
             }
         }
+    }
+
+    private static void sort2(int[] arrays) {
+        int temp;
+        int pos, scan, num = 0;
+        boolean isChange = true;
+        int n = arrays.length;
+        // 外层循环是排序的趟数，需要n-1趟排序
+        for (pos = n - 1; pos > 0 && isChange; pos--) {
+            // 每比较一趟就重新初始化为false
+            isChange = false;
+            num++;
+            // 内存循环是当前趟数需要比较的次数，n个数需要比较n-1次
+            for (scan = 0; scan < pos; scan++) {
+                if (arrays[scan] > arrays[scan + 1]) {
+                    temp = arrays[scan];
+                    arrays[scan] = arrays[scan + 1];
+                    arrays[scan + 1] = temp;
+                    isChange = true;
+                    System.out.println(Arrays.toString(arrays));
+                }
+            }
+        }
+        System.out.println("趟数：" + num);
     }
 
 }
