a2a6faa48817f9d9bdbe2fffc1b0ec642755daae
Sonder <kuqi007@qq.com>
2020-03-23 17:39:19 +0800

    Leetcode No.2：两数相加。

---
+++ src/main/java/com/zor/algorithm/leetcode/AddTwoNumbers.java
@@ -0,0 +1,38 @@
+package com.zor.algorithm.leetcode;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode head = dummyNode;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + carry;
+            head.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            head = head.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return dummyNode.next;
+    }
+
+
+}
---
+++ src/main/java/com/zor/algorithm/leetcode/ListNode.java
@@ -0,0 +1,10 @@
+package com.zor.algorithm.leetcode;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
