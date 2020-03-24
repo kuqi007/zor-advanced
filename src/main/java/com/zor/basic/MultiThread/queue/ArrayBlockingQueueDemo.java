0411f7e98c7efdd7fd2daef1c29b5cc092c38ac0
Sonder <kuqi007@qq.com>
2020-03-24 23:14:19 +0800

    Queue测试

---
+++ src/main/java/com/zor/basic/MultiThread/queue/ArrayBlockingQueueDemo.java
@@ -0,0 +1,9 @@
+package com.zor.basic.MultiThread.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ArrayBlockingQueueDemo {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> integers = new ArrayBlockingQueue<>(3);
+    }
+}
