0bfeb2b579f33cab4543ff3394ad0c76540d1a64
Sonder <kuqi007@qq.com>
2020-08-22 09:34:11 +0800

    一些未完成的事

---
+++ src/main/java/com/zor/algorithm/interview/online/DecodeWays.java
@@ -0,0 +1,25 @@
+package com.zor.algorithm.interview.online;
+
+import java.util.Scanner;
+
+/**
+ *  2020-03月 字节跳动面试
+ * a-z分别对应数字1-26, 给你一串数字(不能改变顺序), 方法得出:一串数字解码成对应的英文字母, 有几种可能性? 
+ *
+ * 123
+ * abc
+ *
+ * 参考 https://leetcode-cn.com/problems/decode-ways/
+ */
+// todo
+public class DecodeWays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        while (a / 10 <= 26 && a / 10 >= 1) {
+
+        }
+
+
+    }
+}
