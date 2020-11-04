78caebf26269e211a8a2d4e0afc096ff1b553885
kuqi007 <kuqi007@qq.com>
2020-11-04 20:57:06 +0800

    一些注释

--- src/main/java/com/zor/algorithm/leetcode/ReverseInteger.java
+++ src/main/java/com/zor/algorithm/leetcode/ReverseInteger.java
@@ -32,40 +32,63 @@ public class ReverseInteger {
 
     /**
      * int范围 -2147483648~2147483647
-     *
+     * <p>
      * 为了便于解释，我们假设 \text{rev}rev 是正数。
-     *
+     * <p>
      * 如果 temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 导致溢出，那么一定有 \text{rev} \geq \frac{INTMAX}{10}rev≥
      * 10
      * INTMAX
      * ​
-     *  。
+     * 。
      * 如果 \text{rev} > \frac{INTMAX}{10}rev>
      * 10
      * INTMAX
      * ​
-     *  ，那么 temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 一定会溢出。
+     * ，那么 temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 一定会溢出。
      * 如果 \text{rev} == \frac{INTMAX}{10}rev==
      * 10
      * INTMAX
      * ​
-     *  ，那么只要 \text{pop} > 7pop>7，temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 就会溢出。
-     *
+     * ，那么只要 \text{pop} > 7pop>7，temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 就会溢出。
+     * <p>
      * 链接：https://leetcode-cn.com/problems/reverse-integer/solution/zheng-shu-fan-zhuan-by-leetcode/
-     *
      */
     public int reverse(int x) {
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
+            /*
+             * 从ans * 10 + pop > MAX_VALUE这个溢出条件来看
+             * 当出现 ans > MAX_VALUE / 10 且 还有pop需要添加 时，则一定溢出
+             * 当出现 ans == MAX_VALUE / 10 且 pop > 7 时，则一定溢出，7是2^31 - 1的个位数
+             *   rev*10，最后一位肯定是0
+             */
             if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > Integer.MAX_VALUE % 10))
                 return 0;
+            /*
+             * 从ans * 10 + pop < MIN_VALUE这个溢出条件来看
+             * 当出现 ans < MIN_VALUE / 10 且 还有pop需要添加 时，则一定溢出
+             * 当出现 ans == MIN_VALUE / 10 且 pop < -8 时，则一定溢出，8是-2^31的个位数
+             */
             if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < Integer.MIN_VALUE % 10))
                 return 0;
             rev = rev * 10 + pop;
         }
 
         return rev;
+    }
+
+    public static int solution2(int x) {
+        long n = 0;
+        while (x != 0) {
+            n = n * 10 + x % 10;
+            x = x / 10;
+        }
+        return (int) n == n ? (int) n : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution2(-123456));
     }
 }
