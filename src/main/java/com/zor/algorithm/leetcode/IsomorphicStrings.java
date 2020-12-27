f0551d25cfd5dda4320de25b9041e5c96c84f9a6
Sonder <kuqi007@qq.com>
2020-12-27 21:33:04 +0800

    feat: No.205. 同构字符串 & No.290 单词规律

---
+++ src/main/java/com/zor/algorithm/leetcode/IsomorphicStrings.java
@@ -0,0 +1,84 @@
+package com.zor.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kuqi0 on 2020/12/27
+ * No.205. 同构字符串
+ * 给定两个字符串 s 和 t，判断它们是否是同构的。
+ * <p>
+ * 如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+ * <p>
+ * 所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "egg", t = "add"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: s = "foo", t = "bar"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: s = "paper", t = "title"
+ * 输出: true
+ * 说明:
+ * 你可以假设 s 和 t 具有相同的长度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/isomorphic-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsomorphicStrings {
+
+    public static void main(String[] args) {
+        boolean isomorphic = isIsomorphic("paper", "title");
+        System.out.println(isomorphic);
+
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+
+        Map<Character, Character> mapS = new HashMap<>();
+
+        Map<Character, Character> mapT = new HashMap<>();
+
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) {
+            char x = s.charAt(i), y = t.charAt(i);
+            if ((mapS.containsKey(x) && mapS.get(x) != y) || (mapT.containsKey(y) && mapT.get(y) != x)) {
+                return false;
+            }
+            mapS.put(x, y);
+            mapT.put(y, x);
+        }
+        return true;
+    }
+
+    /**
+     * 此解法效率要高于上面那一种
+     */
+    public static boolean solution2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                if (map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            } else {
+                if (map.get(s.charAt(i)) != t.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/zor/algorithm/leetcode/WordPattern.java
@@ -0,0 +1,97 @@
+package com.zor.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Created by kuqi0 on 2020/12/27
+ * No.290 单词规律
+ * 给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", str = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", str = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", str = "dog cat cat dog"
+ * 输出: false
+ * 示例 4:
+ * <p>
+ * 输入: pattern = "abba", str = "dog dog dog dog"
+ * 输出: false
+ * 说明:
+ * 你可以假设 pattern 只包含小写字母， str 包含了由单个空格分隔的小写字母。 
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-pattern
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class WordPattern {
+
+    public static void main(String[] args) {
+
+        System.out.println(wordPattern("aaaa", "dog cat cat dog"));
+
+    }
+
+    public static boolean wordPattern(String pattern, String s) {
+
+        int length = pattern.length();
+        String[] split = s.split(" ");
+        if (split.length != length) {
+            return false;
+        }
+
+        Map<Character, String> mapPattern = new HashMap<>();
+        Map<String, Character> mapS = new HashMap<>();
+
+
+        for (int i = 0; i < length; i++) {
+            char p = pattern.charAt(i);
+            String word = split[i];
+
+            if ((mapPattern.containsKey(p) && !mapPattern.get(p).equals(word))
+                    || (mapS.containsKey(word) && mapS.get(word) != p)) {
+                return false;
+            }
+            mapPattern.put(p, word);
+            mapS.put(word, p);
+        }
+        return true;
+    }
+
+    /**
+     * 第二种方法：一个 HashMap 即可。大概思路如下：
+     * 1：我们只管 pattern 与 s 之间字符一一对应即可，看一个例子 pattern=abba，s=dog cat cat dog；第一次调用 map 的 put 方法时返回 null，如果是第 n 次调用的话即返回第 n-1 次的 value，很明显 valua 至关重要，因为 pattern 与 s 的长度是相等的，所以数组下标 i 是一一对应的，我们用 i 做 map 的 value 即可；
+     * 2：所以我们直接遍历 pattern 就可以了，第一次 map.put(a)跟 map.put(dog)都是返回 null，第二次 map.put(a)跟 map.put(dog)都是返回 0，以此类推下去即可；
+     * 3：这种解决问题的方法的重点在于利用两个字符串的下标是一一对应的，其次则是利用put的返回值；
+     * 代码如下
+     *
+     * 作者：zheng-rong-huai
+     * 链接：https://leetcode-cn.com/problems/word-pattern/solution/liang-chong-si-lu-9894-9853-by-zheng-ron-4x63/
+     */
+    private static boolean solution2(String pattern, String s) {
+        int length = pattern.length();
+        String[] split = s.split(" ");
+        if (split.length != length) {
+            return false;
+        }
+        Map<Object, Object> map = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            // 利用map.put方法的返回值（上一次的value）
+            if (!Objects.equals(map.put(pattern.charAt(i), i), map.put(split[i], i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
