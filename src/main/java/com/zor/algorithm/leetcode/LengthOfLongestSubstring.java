70d1d02eb19316eecbe188a03097fb958ecfec15
Sonder <kuqi007@qq.com>
2020-12-27 21:37:23 +0800

    feat: leetcode No.3

--- src/main/java/com/zor/algorithm/leetcode/LengthOfLongestSubstring.java
+++ src/main/java/com/zor/algorithm/leetcode/LengthOfLongestSubstring.java
@@ -1,9 +1,10 @@
 package com.zor.algorithm.leetcode;
 
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
+ * No.3
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
  * <p>
  * 示例 1:
@@ -28,6 +29,13 @@ import java.util.HashSet;
 // TODO
 public class LengthOfLongestSubstring {
 
+    public static void main(String[] args) {
+
+        int count = violentCrack("au");
+        System.out.println(count);
+
+    }
+
     public int lengthOfLongestSubstring(String s) {
         int count = 0;
         while (s.length() > 0) {
@@ -37,5 +45,29 @@ public class LengthOfLongestSubstring {
 
 
         return count;
+    }
+
+    /**
+     * 暴力破解
+     *
+     * @param s
+     * @return
+     */
+    private static int violentCrack(String s) {
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+            boolean isDuplicate = false;
+            for (int j = i; j < s.length(); j++) {
+                if (!set.add(s.charAt(j))) {
+                    // 如果出现重复字符，应当把这个字符的长度去掉，本来为j-i+1，现在应为j-i
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            maxLength = isDuplicate ? set.size() - 1 : set.size();
+
+        }
+        return maxLength;
     }
 }
