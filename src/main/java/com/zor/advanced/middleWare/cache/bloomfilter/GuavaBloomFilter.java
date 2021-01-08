1f7904a7a3d7bc6414884a4c8c117803f94f9e3b
kuqi007 <kuqi007@qq.com>
2021-01-08 15:59:11 +0800

    feat: 布隆过滤器demo

---
+++ src/main/java/com/zor/advanced/middleWare/cache/bloomfilter/GuavaBloomFilter.java
@@ -0,0 +1,31 @@
+package com.zor.advanced.middleWare.cache.bloomfilter;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author zqq
+ * @date 2021/1/8
+ */
+public class GuavaBloomFilter {
+    public static void main(String[] args) {
+
+        BloomFilter<String> filter = BloomFilter.create(
+                Funnels.stringFunnel(Charset.defaultCharset()),
+                1500,
+                0.01);
+
+        System.out.println(filter.mightContain("test1"));
+        System.out.println(filter.mightContain("test2"));
+
+        filter.put("test1");
+        filter.put("test2");
+
+        System.out.println(filter.mightContain("test1"));
+        System.out.println(filter.mightContain("test2"));
+
+    }
+
+}
