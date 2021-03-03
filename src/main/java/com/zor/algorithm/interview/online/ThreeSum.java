05495a89f5d7db450c5d1c2257454977e521de4a
kuqi007 <kuqi007@qq.com>
2021-03-03 21:36:09 +0800

    feat: 三数之和

--- src/main/java/com/zor/algorithm/interview/online/ThreeSum.java
+++ src/main/java/com/zor/algorithm/interview/online/ThreeSum.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 3, 5, 7, 9, 0};
+        int[] arr = new int[]{1, 1, 3, 3, 5, 7, 7, 9, 9, 0};
         List<List<Integer>> sum = getSum(arr, 10);
         System.out.println(sum);
     }
@@ -25,14 +25,14 @@ public class ThreeSum {
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < n - 2; i++) {
             int num1 = nums[i];
-            int left = i + 1, right = i + 2;
+            int left = i + 1, right = n - 1;
             while (left < right) {
                 if (nums[left] + nums[right] + num1 == target) {
                     List<Integer> temp = Lists.newArrayList(num1, nums[left], nums[right]);
                     ans.add(temp);
                 }
                 left++;
-                right++;
+                right--;
             }
         }
         return ans;
