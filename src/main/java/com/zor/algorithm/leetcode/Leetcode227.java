19d1b64f7817693b7fa16e9d9cbd064de213e629
Sonder <kuqi007@qq.com>
2021-03-11 23:30:11 +0800

    feat: Leetcode227

---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode227.java
@@ -0,0 +1,88 @@
+package com.zor.algorithm.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 227. 基本计算器 II
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ * <p>
+ * 整数除法仅保留整数部分。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "3+2*2"
+ * 输出：7
+ * 示例 2：
+ * <p>
+ * 输入：s = " 3/2 "
+ * 输出：1
+ * 示例 3：
+ * <p>
+ * 输入：s = " 3+5 / 2 "
+ * 输出：5
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 3 * 105
+ * s 由整数和算符 ('+', '-', '*', '/') 组成，中间由一些空格隔开
+ * s 表示一个 有效表达式
+ * 表达式中的所有整数都是非负整数，且在范围 [0, 231 - 1] 内
+ * 题目数据保证答案是一个 32-bit 整数
+ * Created by kuqi0 on 2021/3/11
+ */
+public class Leetcode227 {
+
+    public static void main(String[] args) {
+
+        String s = "3+2*2";
+        int calculate = calculate(s);
+        System.out.println(calculate);
+
+
+    }
+
+    /**
+     * TODO没太理解
+     * @param s
+     * @return
+     */
+    public static int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                num = num * 10 + ch - '0';
+            }
+            if (!Character.isDigit(ch) && ch != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = ch;
+                num = 0;
+            }
+        }
+
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
