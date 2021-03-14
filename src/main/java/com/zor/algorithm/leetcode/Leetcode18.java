83d4ed5cbf6425a4ca6d6f5304a6ff3b2df47d5b
Sonder <kuqi007@qq.com>
2021-03-15 01:06:36 +0800

    feat: Leetcode18四数之和

--- src/main/java/com/zor/algorithm/leetcode/Leetcode18.java
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode18.java
@@ -1,5 +1,7 @@
 package com.zor.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -29,17 +31,116 @@ import java.util.List;
  */
 public class Leetcode18 {
     public static void main(String[] args) {
+        int[] nums = {-2, -1, -1, 1, 1, 2, 2};
+        List<List<Integer>> lists = fourSum(nums, 0);
+        System.out.println(lists);
+
+    }
+
+    private static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return ans;
+        }
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            // 如果这个数加上最大的三个数小于target，那么直接进入下次循环
+            if (nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                // 如果这个数加上最大的三个数小于target，那么直接进入下次循环
+                if (nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+
+                int l = j + 1, r = length - 1;
+                while (l < r) {
+                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        l++;
+                        while (l < r && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        r--;
+                    } else if (sum < target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        return ans;
 
     }
 
     /**
-     * TODO
-     * @param nums
-     * @param target
-     * @return
+     * 四数之和转换为三数之和
      */
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    public static List<List<Integer>> solution1(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n - 3; i++) {
+            int cur = nums[i];
+            if (i > 0 && cur == nums[i - 1]) {
+                continue;
+            }
+            List<List<Integer>> threeSum = threeSum(nums, i + 1, target - cur);
+            for (List<Integer> sum : threeSum) {
+                sum.add(0, cur);
+                ans.add(sum);
+            }
+        }
+        return ans;
+    }
 
-        return null;
+    /**
+     * 通用三数之和解法
+     */
+    private static List<List<Integer>> threeSum(int[] nums, int start, int target) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = start; i < n - 2; i++) {
+            int cur = nums[i];
+            // 这里i需要大于start，不要和start之前元素比较
+            if (i > start && cur == nums[i - 1]) {
+                continue;
+            }
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = cur + nums[l] + nums[r];
+                if (sum < target) {
+                    l++;
+                } else if (sum > target) {
+                    r--;
+                } else {
+                    ans.add(new ArrayList<>(Arrays.asList(cur, nums[l], nums[r])));
+                    while (l < r && nums[l + 1] == nums[l]) l++;
+                    while (l < r && nums[r - 1] == nums[r]) r--;
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return ans;
     }
 }
