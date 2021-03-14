35789a9bb775b804755ed4aed08d3b6eb5722c1b
Sonder <kuqi007@qq.com>
2021-03-14 23:28:22 +0800

    feat: Leetcode18

---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode18.java
@@ -0,0 +1,45 @@
+package com.zor.algorithm.leetcode;
+
+import java.util.List;
+
+/**
+ * 18. 四数之和
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+ * <p>
+ * 注意：答案中不可以包含重复的四元组。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,0,-1,0,-2,2], target = 0
+ * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [], target = 0
+ * 输出：[]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 200
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Created by kuqi0 on 2021/3/14
+ */
+public class Leetcode18 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * TODO
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        return null;
+    }
+}
