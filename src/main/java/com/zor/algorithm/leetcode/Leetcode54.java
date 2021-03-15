ed2a7dcef8b8f1544a19c26a6252bfbed3b8b467
kuqi007 <kuqi007@qq.com>
2021-03-15 21:29:18 +0800

    feat: Leetcode54

---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode54.java
@@ -0,0 +1,53 @@
+package com.zor.algorithm.leetcode;
+
+import java.util.List;
+
+/**
+ * 54. 螺旋矩阵
+ * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 10
+ * -100 <= matrix[i][j] <= 100
+ *
+ * @author zqq
+ * @date 2021/3/15
+ */
+public class Leetcode54 {
+    public static void main(String[] args) {
+
+
+    }
+
+    /**
+     * TODO
+     *
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int h = matrix.length;
+        int w = matrix[0].length;
+
+
+    }
+
+
+}
