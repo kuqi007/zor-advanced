32779cbf93323a35259b0687a83ba1eed05f139c
kuqi007 <kuqi007@qq.com>
2021-03-16 17:08:56 +0800

    feat: TODO Leetcode59

---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode59.java
@@ -0,0 +1,34 @@
+package com.zor.algorithm.leetcode;
+
+/**
+ * 59. 螺旋矩阵 II
+ * 给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：n = 3
+ * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * 示例 2：
+ * <p>
+ * 输入：n = 1
+ * 输出：[[1]]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 20
+ *
+ * @author zqq
+ * @date 2021/3/16
+ */
+public class Leetcode59 {
+
+    public int[][] generateMatrix(int n) {
+        // TODO
+        return new int[0][];
+    }
+
+}
