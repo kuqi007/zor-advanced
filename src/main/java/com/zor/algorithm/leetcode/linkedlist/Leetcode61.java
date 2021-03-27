cd6bb9424947ebb91a59b7fb582cc639a0278a66
Sonder <kuqi007@qq.com>
2021-03-27 23:45:46 +0800

    feat: Leetcode61

---
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode61.java
@@ -0,0 +1,41 @@
+package com.zor.algorithm.leetcode.linkedlist;
+
+import com.zor.algorithm.leetcode.linkedlist.base.ListNode;
+
+/**
+ * 61. 旋转链表
+ * 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[4,5,1,2,3]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [0,1,2], k = 4
+ * 输出：[2,0,1]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目在范围 [0, 500] 内
+ * -100 <= Node.val <= 100
+ * 0 <= k <= 2 * 10^9
+ * <p>
+ * Created by kuqi0 on 2021/3/27
+ */
+public class Leetcode61 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        // TODO
+
+        return head;
+    }
+}
