3933125a09f1bdab619ea0a303602b5566193b66
kuqi007 <kuqi007@qq.com>
2021-03-30 10:39:30 +0800

    feat: Leetcode74

---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode74.java
@@ -0,0 +1,40 @@
+package com.zor.algorithm.leetcode;
+
+/**
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ * <p>
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ * 输出：false
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 100
+ * -104 <= matrix[i][j], target <= 104
+ * <p>
+ *
+ * @author zqq
+ * @date 2021/3/30
+ */
+public class Leetcode74 {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //
+
+        return false;
+    }
+}
