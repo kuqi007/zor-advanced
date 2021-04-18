ec42f3b2a93b8910335f4299bb31783b3969383a
Sonder <kuqi007@qq.com>
2021-04-19 00:02:18 +0800

    feat: Leetcode876

---
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode876.java
@@ -0,0 +1,53 @@
+package com.zor.algorithm.leetcode.linkedlist;
+
+import com.zor.algorithm.leetcode.linkedlist.base.ListNode;
+import com.zor.algorithm.leetcode.linkedlist.base.ListNodeUtil;
+
+/**
+ * 876. 链表的中间结点
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ * <p>
+ * 如果有两个中间结点，则返回第二个中间结点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ * 示例 2：
+ * <p>
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 给定链表的结点数介于 1 和 100 之间。
+ * Created by kuqi0 on 2021/4/18
+ */
+public class Leetcode876 {
+    public static void main(String[] args) {
+        ListNode listNode = ListNodeUtil.getListNode(1, 2, 3, 4, 5);
+        ListNode listNode1 = middleNode(listNode);
+        ListNodeUtil.printList(listNode1);
+
+    }
+
+    /**
+     * 快慢指针法
+     */
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
