daa239d1208ba82d74106665d9030fa4ee404cfb
Sonder <kuqi007@qq.com>
2021-04-20 23:16:03 +0800

    feat: Leetcode344

---
+++ src/main/java/com/zor/algorithm/geekbang/stack/ArrayStack.java
@@ -0,0 +1,17 @@
+package com.zor.algorithm.geekbang.stack;
+
+/**
+ * 基于数组实现的栈
+ * Created by kuqi0 on 2021/4/20
+ */
+public class ArrayStack {
+
+    // 数组
+    private String[] items;
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode344.java
@@ -0,0 +1,48 @@
+package com.zor.algorithm.leetcode;
+
+
+/**
+ * 344. 反转字符串
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ * <p>
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * <p>
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ * 示例 2：
+ * <p>
+ * 输入：["H","a","n","n","a","h"]
+ * 输出：["h","a","n","n","a","H"]
+ * Created by kuqi0 on 2021/4/20
+ */
+public class Leetcode344 {
+
+    public static void main(String[] args) {
+
+        reverseString("hello".toCharArray());
+
+    }
+
+    public static void reverseString(char[] s) {
+
+        int n = s.length;
+        int i = n - 1, j = 0;
+        while (i >= 0 && j < i) {
+            char temp = s[j];
+            s[j] = s[i];
+            s[i] = temp;
+            j++;
+            i--;
+        }
+        System.out.println(new String(s));
+
+    }
+
+
+}
