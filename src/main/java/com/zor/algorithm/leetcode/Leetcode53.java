68bd0d9a32cb8279ee56cb828f2e1a41979f6b1b
kuqi007 <kuqi007@qq.com>
2021-04-22 15:46:43 +0800

    feat: Leetcode53

--- src/main/java/com/zor/algorithm/leetcode/Leetcode53.java
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode53.java
@@ -3,47 +3,70 @@ package com.zor.algorithm.leetcode;
 /**
  * 53. 最大子序和
  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
  * 输出：6
  * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [1]
  * 输出：1
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [0]
  * 输出：0
  * 示例 4：
- *
+ * <p>
  * 输入：nums = [-1]
  * 输出：-1
  * 示例 5：
- *
+ * <p>
  * 输入：nums = [-100000]
  * 输出：-100000
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= nums.length <= 3 * 104
  * -105 <= nums[i] <= 105
- *
- *
+ * <p>
+ * <p>
  * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
- * @author zhuqiqi03
- * @date 2021/4/22
+ * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ *
+ * @author zqq
+ * @date 2021/1/27
  */
 public class Leetcode53 {
 
-    public int maxSubArray(int[] nums) {
+    public static void main(String[] args) {
 
-        return 0;
     }
 
+    /**
+     * TODO 有点懵
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray(int[] nums) {
+        // ans是结果
+        int ans = nums[0];
+        // sum是最大序列之和
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum = +num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+
+        return ans;
+    }
 }
