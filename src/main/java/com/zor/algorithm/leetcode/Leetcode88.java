1d2cdd1c729868d6568b6ff91ece19664c37a5c7
Sonder <kuqi007@qq.com>
2021-04-23 08:55:18 +0800

    feat: Leetcode88

--- src/main/java/com/zor/algorithm/leetcode/Leetcode88.java
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode88.java
@@ -34,10 +34,64 @@ public class Leetcode88 {
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
-        merge(nums1, 3, nums2, 3);
+        solution0(nums1, 3, nums2, 3);
         System.out.println(Arrays.toString(nums1));
 
     }
+
+    /**
+     * 逆向双指针，空间复杂度为O(1)
+     */
+    public static void solution0(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int tail = m + n - 1;
+        int tmp;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 < 0) {
+                tmp = nums2[p2--];
+            } else if (p2 < 0) {
+                tmp = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                tmp = nums1[p1--];
+            } else {
+                tmp = nums2[p2--];
+            }
+            nums1[tail--] = tmp;
+        }
+    }
+
+    /**
+     * 额外空间
+     */
+    public static void solution1(int[] nums1, int m, int[] nums2, int n) {
+        int[] sorted = new int[m + n];
+        //int index = 0;
+        int p1 = 0, p2 = 0;
+        while (p1 < m || p2 < n) {
+            int tmp;
+            if (p1 == m) {
+                tmp = nums2[p2];
+                p2++;
+            } else if (p2 == n) {
+                tmp = nums1[p1];
+                p1++;
+            } else if (nums1[p1] < nums2[p2]) {
+                tmp = nums1[p1];
+                p1++;
+            } else {
+                tmp = nums2[p2];
+                p2++;
+            }
+            //sorted[index++] = tmp;
+            // 每次p1和p2只有一个指针会移动
+            sorted[p1 + p2 - 1] = tmp;
+        }
+        for (int i = 0; i != m + n; ++i) {
+            nums1[i] = sorted[i];
+        }
+        //System.arraycopy(sorted, 0, nums1, 0, m + n);
+    }
+
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1, j = n - 1;
