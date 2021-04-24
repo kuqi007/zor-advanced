1cf97dfc0127705ba69fa718c83c89fabe213aa9
Sonder <kuqi007@qq.com>
2021-04-24 22:54:36 +0800

    feat: Leetcode234

---
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode234.java
@@ -0,0 +1,28 @@
+package com.zor.algorithm.leetcode.linkedlist;
+
+import com.zor.algorithm.leetcode.linkedlist.base.ListNode;
+
+/**
+ * 234. 回文链表
+ * 请判断一个链表是否为回文链表。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ * <p>
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * Created by kuqi0 on 2021/4/24
+ */
+public class Leetcode234 {
+
+    public boolean isPalindrome(ListNode head) {
+
+        return false;
+    }
+
+}
