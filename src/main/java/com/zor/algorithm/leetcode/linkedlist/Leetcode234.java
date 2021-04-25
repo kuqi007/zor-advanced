9f7eb96195203a82f49b88fcdf57a64a01d6a188
Sonder <kuqi007@qq.com>
2021-04-25 08:54:16 +0800

    feat: Leetcode234 回文链表

--- src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode234.java
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode234.java
@@ -2,6 +2,9 @@ package com.zor.algorithm.leetcode.linkedlist;
 
 import com.zor.algorithm.leetcode.linkedlist.base.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 234. 回文链表
  * 请判断一个链表是否为回文链表。
@@ -20,9 +23,113 @@ import com.zor.algorithm.leetcode.linkedlist.base.ListNode;
  */
 public class Leetcode234 {
 
-    public boolean isPalindrome(ListNode head) {
 
-        return false;
+    /**
+     * TODO 这个没太看懂
+     * 使用快慢两个指针找到链表中点，慢指针每次前进一步，快指针每次前进两步。在慢指针前进的过程中，同时修改其 next 指针，使得链表前半部分反序。最后比较中点两侧的链表是否相等
+     */
+    public boolean solution2(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (slow != null) {
+            if (slow.val != prev.val) {
+                return false;
+            }
+            slow = slow.next;
+            prev = prev.next;
+        }
+
+        return true;
+    }
+
+    public boolean solution1(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        // 找到前半部分链表的尾节点并反转后半部分链表
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        // 将后半部分反转
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // 判断是否回文
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        // 还原链表并返回结果
+        firstHalfEnd.next = reverseList(secondHalfStart);
+
+        return result;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        // 这样判断找中间节点，如果有两个，那么找到的是第一个
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> arr = new ArrayList<>();
+        ListNode cur = head;
+        while (cur != null) {
+            arr.add(cur.val);
+            cur = cur.next;
+        }
+
+        int left = 0, right = arr.size() - 1;
+        while (left < right) {
+            if (!arr.get(left).equals(arr.get(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+
+        return true;
     }
 
 }
