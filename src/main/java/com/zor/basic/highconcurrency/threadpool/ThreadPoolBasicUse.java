c8e7e7603abf497238e2ed1218ba3af7f07311a1
Sonder <kuqi007@qq.com>
2021-04-27 18:19:30 +0800

    feat: 线程池简单使用

--- src/main/java/com/zor/basic/highconcurrency/threadpool/ThreadPoolBasicUse.java
+++ src/main/java/com/zor/basic/highconcurrency/threadpool/ThreadPoolBasicUse.java
@@ -1,5 +1,6 @@
 package com.zor.basic.highconcurrency.threadpool;
 
+import java.text.NumberFormat;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Future;
@@ -11,24 +12,48 @@ import java.util.concurrent.TimeUnit;
  * @date 2021/4/26
  */
 public class ThreadPoolBasicUse {
+    private static final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10,
+            1, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(10));
 
     public static void main(String[] args) {
-        TimeUnit unit = TimeUnit.MILLISECONDS;
-        BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(2);
-        ThreadPoolExecutor threadPoolExecutor =
-                new ThreadPoolExecutor(2, 5, 1, unit, workQueue);
-
-
         // 核心线程和非核心线程不分类，一视同仁
         // 会出现一个问题，后来的任务先执行，因为先来的一部分会到队列排队
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 20; i++) {
             Command command = new Command(i);
             // submit会有返回值
-            Future<?> submit = threadPoolExecutor.submit(command);
+            //Future<?> submit = threadPoolExecutor.submit(command);
             // execute没有返回值
             threadPoolExecutor.execute(command);
         }
 
+        // 模拟动态控制线程池
+        int corePoreSize = 10;
+        int maxPoolSize = 20;
+        threadPoolExecutor.setCorePoolSize(corePoreSize);
+        threadPoolExecutor.setMaximumPoolSize(maxPoolSize);
+
+        // 模拟监控线程池负载情况
+        Thread command = new Thread(() -> {
+            while (true){
+                getLoadPercent();
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        threadPoolExecutor.submit(command);
+
+
+    }
+
+
+    public static Double getLoadPercent() {
+        int activeCount = threadPoolExecutor.getActiveCount();
+        int maximumPoolSize = threadPoolExecutor.getMaximumPoolSize();
+        System.out.println("线程池负载为：" + 1.0 * activeCount / maximumPoolSize * 100 + "%");
+        return 1.0 * activeCount / maximumPoolSize;
     }
 
     static class Command implements Runnable {
