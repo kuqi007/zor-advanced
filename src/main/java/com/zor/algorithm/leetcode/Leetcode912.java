b562d174df3e26b4382b5e6283e960a3ee7e6307
Sonder <kuqi007@qq.com>
2021-05-04 14:12:50 +0800

    feat: 随机化快排

--- src/main/java/com/zor/algorithm/leetcode/Leetcode912.java
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode912.java
@@ -1,8 +1,7 @@
 package com.zor.algorithm.leetcode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Random;
 
 /**
  * 912. 排序数组
@@ -35,26 +34,33 @@ public class Leetcode912 {
 
     private void quickSort(int[] arr, int left, int right) {
         if (left < right) {
-            int partitionIndex = partition(arr, left, right);
+            int partitionIndex = randomPartition(arr, left, right);
             quickSort(arr, left, partitionIndex - 1);
             quickSort(arr, partitionIndex + 1, right);
         }
     }
 
+    /**
+     * 随机化快排，在数组已经有序的情况下，打破有序
+     */
+    private int randomPartition(int[] arr, int left, int right) {
+        int i = new Random().nextInt(right - left + 1) + left;
+        swap(arr, right, i);
+        return partition(arr, left, right);
+    }
+
     private int partition(int[] arr, int left, int right) {
-        // 设定基准值
-        int pivot = left;
-        int index = pivot + 1;
-        for (int i = index; i <= right; i++) {
-            if (arr[i] < arr[pivot]) {
-                swap(arr, i, index);
-                index++;
+        // 选定右边作为基准值
+        int pivot = arr[right];
+        int i = left;
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                swap(arr, i, j);
+                i++;
             }
         }
-        swap(arr, pivot, index - 1);
-
-        return index - 1;
-
+        swap(arr, i, right);
+        return i;
     }
 
 
