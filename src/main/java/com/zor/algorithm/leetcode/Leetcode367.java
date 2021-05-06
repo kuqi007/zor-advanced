f4ee05bb3f2e4a4fd5c318272f3cbf55c5423c66
Sonder <kuqi007@qq.com>
2021-05-06 08:51:51 +0800

    feat: Leetcode367

---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode367.java
@@ -0,0 +1,31 @@
+package com.zor.algorithm.leetcode;
+
+/**
+ * Created by kuqi0 on 2021/5/6
+ */
+public class Leetcode367 {
+    public static void main(String[] args) {
+        Leetcode367 leetcode367 = new Leetcode367();
+        boolean perfectSquare = leetcode367.isPerfectSquare(1);
+        System.out.println(perfectSquare);
+
+    }
+
+    public boolean isPerfectSquare(int num) {
+        // 防止溢出
+        // r=num 可以覆盖数字0和1的情况
+        long l = 0, r = num, x;
+        while (l <= r) {
+            x = (l + r) >>> 1;
+            long guessSquared = x * x;
+            if (guessSquared == num) {
+                return true;
+            } else if (guessSquared > num) {
+                r = x - 1;
+            } else {
+                l = x + 1;
+            }
+        }
+        return false;
+    }
+}
