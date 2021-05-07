df63483916b7681041b18ac395f02681d9595072
Sonder <kuqi007@qq.com>
2021-05-07 21:44:17 +0800

    feat: Leetcode104

---
+++ src/main/java/com/zor/algorithm/leetcode/tree/Leetcode104.java
@@ -0,0 +1,34 @@
+package com.zor.algorithm.leetcode.tree;
+
+/**
+ * 104. 二叉树的最大深度
+ * 给定一个二叉树，找出其最大深度。
+ * <p>
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3 。
+ * Created by kuqi0 on 2021/5/7
+ */
+public class Leetcode104 {
+
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+
+    }
+}
