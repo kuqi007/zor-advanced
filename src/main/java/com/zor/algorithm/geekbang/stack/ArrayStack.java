b117ecd2dd93814a93daa3c04c50afb385caed33
Sonder <kuqi007@qq.com>
2021-05-08 08:02:51 +0800

    feat: ArrayStack

--- src/main/java/com/zor/algorithm/geekbang/stack/ArrayStack.java
+++ src/main/java/com/zor/algorithm/geekbang/stack/ArrayStack.java
@@ -5,9 +5,38 @@ package com.zor.algorithm.geekbang.stack;
  * Created by kuqi0 on 2021/4/20
  */
 public class ArrayStack {
-
     // 数组
-    private String[] items;
+    private final String[] items;
+    // 栈的大小
+    private final int n;
+    // 栈中元素的个数
+    private int count;
+
+    // 初始化数组，申请一个大小为n的数组空间
+    public ArrayStack(int n) {
+        this.items = new String[n];
+        this.n = n;
+        this.count = 0;
+    }
+
+    public boolean push(String item) {
+        // 数组空间不够了，直接返回false，入栈失败
+        if (count == n) return false;
+        // 将item放到下标为count的位置，并且将count加一
+        items[count] = item;
+        ++count;
+        return true;
+    }
+
+    // 出栈操作
+    public String pop() {
+        // 栈为空，则直接返回null
+        if (count == 0) return null;
+        // 返回下标为count-1的数组元素，并且栈中元素个数count减一
+        String tmp = items[count - 1];
+        --count;
+        return tmp;
+    }
 
     public static void main(String[] args) {
 
