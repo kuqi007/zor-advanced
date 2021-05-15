8752c1e2bdb6eee2641b26d27f54029734449398
Sonder <kuqi007@qq.com>
2021-05-15 16:32:37 +0800

    feat: Leetcode25 k个一组翻转链表

---
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode25.java
@@ -0,0 +1,96 @@
+package com.zor.algorithm.leetcode.linkedlist;
+
+import com.zor.algorithm.leetcode.linkedlist.base.ListNode;
+import com.zor.algorithm.leetcode.linkedlist.base.ListNodeUtil;
+
+/**
+ * 25. K 个一组翻转链表
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * <p>
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ * 进阶：
+ * <p>
+ * 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ * 示例 3：
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 1
+ * 输出：[1,2,3,4,5]
+ * 示例 4：
+ * <p>
+ * 输入：head = [1], k = 1
+ * 输出：[1]
+ * 提示：
+ * <p>
+ * 列表中节点的数量在范围 sz 内
+ * 1 <= sz <= 5000
+ * 0 <= Node.val <= 1000
+ * 1 <= k <= sz
+ * Created by kuqi0 on 2021/5/15
+ */
+public class Leetcode25 {
+
+    public static void main(String[] args) {
+        Leetcode25 leetcode25 = new Leetcode25();
+        ListNode listNode = ListNodeUtil.getListNode(1, 2, 3, 4, 5);
+        ListNode ans = leetcode25.reverseKGroup(listNode, 2);
+
+        ListNodeUtil.printList(ans);
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummyNode = new ListNode(-1, head);
+        ListNode pre = dummyNode;
+        ListNode end = dummyNode;
+
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) break;
+
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            // 将链表拆开，进行反转，否则反转的将不止本次链表
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+
+            pre = start;
+            end = start;
+        }
+        return dummyNode.next;
+    }
+
+
+    /**
+     * 标准链表翻转
+     */
+    private ListNode reverse(ListNode head) {
+        ListNode cur = head, pre = null;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+
+}
