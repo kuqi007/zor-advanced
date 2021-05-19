fa9ff4f8b38aa9618a49dababda92566bcd000e0
Sonder <kuqi007@qq.com>
2021-05-19 21:55:27 +0800

    feat: Leetcode23 合并K个升序链表

---
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode23.java
@@ -0,0 +1,96 @@
+package com.zor.algorithm.leetcode.linkedlist;
+
+import com.zor.algorithm.leetcode.linkedlist.base.ListNode;
+import com.zor.algorithm.leetcode.linkedlist.base.ListNodeUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 23. 合并K个升序链表
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * <p>
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ * 示例 2：
+ * <p>
+ * 输入：lists = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：lists = [[]]
+ * 输出：[]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * Created by kuqi0 on 2021/5/16
+ */
+public class Leetcode23 {
+
+    public static void main(String[] args) {
+        Leetcode23 leetcode23 = new Leetcode23();
+        ListNode l1 = ListNodeUtil.getListNode(1, 4, 5);
+        ListNode l2 = ListNodeUtil.getListNode(1, 3, 4);
+        ListNode l3 = ListNodeUtil.getListNode(2, 6);
+        ListNode[] listNodes = {l1, l2, l3};
+        ListNode res = leetcode23.mergeKLists(listNodes);
+        ListNodeUtil.printList(res);
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode res = null;
+        for (ListNode list : lists) {
+            res = merge(res, list);
+        }
+        return res;
+    }
+
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode cur = dummyNode;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else if (l1.val > l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+        return dummyNode.next;
+    }
+
+}
