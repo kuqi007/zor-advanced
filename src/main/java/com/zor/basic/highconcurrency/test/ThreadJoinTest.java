afb75afb34d8df7a1c69ff431a751e69fa07fad4
Sonder <kuqi007@qq.com>
2021-05-22 23:54:52 +0800

    feat: 多线程相关测试

---
+++ src/main/java/com/zor/basic/highconcurrency/test/ThreadJoinTest.java
@@ -0,0 +1,43 @@
+package com.zor.basic.highconcurrency.test;
+
+/**
+ * Created by kuqi0 on 2021/5/22
+ */
+public class ThreadJoinTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println("线程1运行-------");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread1.start();
+
+
+        Thread thread2 = new Thread(() -> {
+            System.out.println("线程2运行-------");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread2.start();
+
+        System.out.println("主线程等待子线程执行=====");
+        long l = System.currentTimeMillis();
+
+        thread1.join();
+        thread2.join();
+        long l1 = System.currentTimeMillis();
+        System.out.println("等待结束，耗时：" + (l1 - l) + "ms");
+
+
+    }
+
+}
