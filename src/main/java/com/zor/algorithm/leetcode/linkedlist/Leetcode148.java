4f617b2f74fcde67d4b9ce4f3cff149066e95fcc
kuqi007 <kuqi007@qq.com>
2021-06-02 23:28:16 +0800

    feat: Leetcode148 排序链表

--- src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode148.java
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode148.java
@@ -41,32 +41,76 @@ public class Leetcode148 {
 
     public static void main(String[] args) {
         Leetcode148 leetcode148 = new Leetcode148();
-        ListNode listNode1 = ListNodeUtil.getListNode(5, 4, 3, 2, 1, 0, -1);
+        ListNode listNode1 = ListNodeUtil.getListNode(5, 4, 3, 2, 1, 0);
         ListNode listNode2 = ListNodeUtil.getListNode(-1, 5, 3, 4, 0);
         ListNode res = leetcode148.mergeSort(listNode1);
         ListNodeUtil.printList(res);
     }
 
+    /**
+     * 如果slow = fast = head,那么在链表只剩两个节点的时候有以下执行顺序：(首节点->尾节点->null)
+     * 0. 执行查找中点的while循环
+     * 1. 得出：slow = 尾节点，fast = null
+     * 2. mid = slow.next;
+     * slow.next = null;
+     * //第2步为断链,需要注意的是,slow的next原本就是null(由第1步得出)
+     * //也就是说,断链操作根本没有生效,链表结构依然是:首节点->尾节点->null
+     * 3. 进入递归,由于链表结构没变,所以会继续进行查找中点、断链操作,重复以上步骤,导致栈溢出
+     * -----------------------------------------------------------
+     * 如果slow = head,fast = head.next,那么在链表只剩两个节点的时候有以下执行顺序：(首节点->尾节点->null)
+     * 0. 执行查找中点的while循环
+     * 1. 得出：slow = 首节点，fast = 尾节点
+     * 2. mid = slow.next;
+     * slow.next = null;
+     * //这里的断链操作生效了,此时链表结构为：首节点->null,尾节点->null
+     * 3. 进入递归,重复以上步骤,此时的链表已经分解成两个节点,所以不会查找中点及断链,而是直接return。
+     */
     public ListNode sortList(ListNode head) {
-        return mergeSort(head);
+        if (head == null || head.next == null)
+            return head;
+        // 这里需要注意
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
     }
+
 
     /**
      * 自顶向下的归并排序
      */
     private ListNode mergeSort(ListNode head) {
-        if (head.next == null) return head;
-        ListNode p = head;
-        ListNode q = head;
+        if (head == null || head.next == null) return head;
+        ListNode fast = head;
+        ListNode slow = head;
         ListNode pre = null;
-        while (p != null && p.next != null) {
-            pre = q;
-            p = p.next.next;
-            q = q.next;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            fast = fast.next.next;
+            slow = slow.next;
         }
         pre.next = null;
         ListNode l1 = mergeSort(head);
-        ListNode l2 = mergeSort(q);
+        ListNode l2 = mergeSort(slow);
         return merge(l1, l2);
     }
 
