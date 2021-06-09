8568db8c33f3e6bcde8d35d2316c663ff39184ad
kuqi007 <kuqi007@qq.com>
2021-06-09 17:53:30 +0800

    feat: ReadWriteLockDemo

---
+++ src/main/java/com/zor/basic/highconcurrency/lock/ReadWriteLockDemo.java
@@ -0,0 +1,29 @@
+package com.zor.basic.highconcurrency.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author zhuqiqi03
+ * @date 2021/6/9
+ */
+public class ReadWriteLockDemo {
+
+
+    public static void main(String[] args) {
+        // TODO: 2021/6/9  
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        Lock lock = readWriteLock.readLock();
+
+        try {
+            lock.lock();
+        } finally {
+            lock.unlock();
+        }
+
+
+    }
+
+
+}
