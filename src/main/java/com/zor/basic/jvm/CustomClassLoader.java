78ec6c1b7d61d4f3c42cdfd98f5b2db5a41842ab
Sonder <kuqi007@qq.com>
2021-06-11 00:22:55 +0800

    feat: 自定义类加载器

---
+++ src/main/java/com/zor/basic/jvm/CustomClassLoader.java
@@ -0,0 +1,44 @@
+package com.zor.basic.jvm;
+
+import org.checkerframework.checker.units.qual.C;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Created by kuqi0 on 2021/6/11
+ */
+public class CustomClassLoader extends ClassLoader {
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        try {
+            byte[] result = getClassFromCustomPath(name);
+            if (result == null) {
+                throw new FileNotFoundException();
+            } else {
+                return defineClass(name, result, 0, result.length);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return super.findClass(name);
+    }
+
+    private byte[] getClassFromCustomPath(String name) {
+        // TODO: 2021/6/11
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        CustomClassLoader customClassLoader = new CustomClassLoader();
+        try {
+            Class<?> clazz = Class.forName("One", true, customClassLoader);
+            Object obj = clazz.newInstance();
+            System.out.println(obj.getClass().getClassLoader());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
