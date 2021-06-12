13edc7d278a45d1a8859b6ae25ea3be53e7c31f6
Sonder <kuqi007@qq.com>
2021-06-12 11:46:15 +0800

    feat: Leetcode1114 按需打印

--- src/main/java/com/zor/algorithm/leetcode/concurrency/Leetcode1114.java
+++ src/main/java/com/zor/algorithm/leetcode/concurrency/Leetcode1114.java
@@ -1,4 +1,4 @@
-package com.zor.algorithm.leetcode;
+package com.zor.algorithm.leetcode.concurrency;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
