0c1862b4c16aae1bc35fae2bbf8da86a947800d1
kuqi007 <kuqi007@qq.com>
2021-06-15 00:44:31 +0800

    feat: Leetcode852 脉数组的峰顶索引

---
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode852.java
@@ -0,0 +1,90 @@
+package com.zor.algorithm.leetcode;
+
+/**
+ * 852. 山脉数组的峰顶索引
+ * 符合下列属性的数组 arr 称为 山脉数组 ：
+ * arr.length >= 3
+ * 存在 i（0 < i < arr.length - 1）使得：
+ * arr[0] < arr[1] < ... arr[i-1] < arr[i]
+ * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ * 给你由整数组成的山脉数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = [0,1,0]
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：arr = [0,2,1,0]
+ * 输出：1
+ * 示例 3：
+ * <p>
+ * 输入：arr = [0,10,5,2]
+ * 输出：1
+ * 示例 4：
+ * <p>
+ * 输入：arr = [3,4,5,1]
+ * 输出：2
+ * 示例 5：
+ * <p>
+ * 输入：arr = [24,69,100,99,79,78,67,36,26,19]
+ * 输出：2
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 3 <= arr.length <= 104
+ * 0 <= arr[i] <= 106
+ * 题目数据保证 arr 是一个山脉数组
+ * <p>
+ * <p>
+ * 进阶：很容易想到时间复杂度 O(n) 的解决方案，你可以设计一个 O(log(n)) 的解决方案吗？
+ *
+ * @author zhuqiqi03
+ * @date 2021/6/15
+ */
+public class Leetcode852 {
+
+    public int solution3(int[] arr) {
+        int low = 0, high = arr.length;
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int low = 0, high = arr.length;
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1]) {
+                low = mid + 1;
+            } else if (arr[mid] < arr[mid - 1]) {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    /**
+     * 暴力解
+     */
+    public int solution1(int[] arr) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            // 因为前面肯定是递增的，遇到第一个递减的数即为结果
+            if (arr[i] > arr[i + 1]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
