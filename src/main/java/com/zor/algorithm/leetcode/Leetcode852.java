cf56c4a445d04dc6470a2e84a33c4694e233a0b8
kuqi007 <kuqi007@qq.com>
2021-06-15 00:48:24 +0800

    feat: Leetcode852 脉数组的峰顶索引

--- src/main/java/com/zor/algorithm/leetcode/Leetcode852.java
+++ src/main/java/com/zor/algorithm/leetcode/Leetcode852.java
@@ -60,6 +60,19 @@ public class Leetcode852 {
         return low;
     }
 
+    public int solution2(int[] arr) {
+        int low = 0, high = arr.length;
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            if (arr[mid] > arr[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+
     public int peakIndexInMountainArray(int[] arr) {
         int low = 0, high = arr.length;
         while (low < high) {
