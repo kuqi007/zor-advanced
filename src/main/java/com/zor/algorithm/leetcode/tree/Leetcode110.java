8a8d41db32379e9e17c230da810eb8ea2c2edbbc
Sonder <kuqi007@qq.com>
2021-08-10 09:38:08 +0800

    feat: Leetcode110

--- src/main/java/com/zor/algorithm/leetcode/tree/Leetcode110.java
+++ src/main/java/com/zor/algorithm/leetcode/tree/Leetcode110.java
@@ -35,22 +35,37 @@ package com.zor.algorithm.leetcode.tree;
  */
 public class Leetcode110 {
 
+
+    /**
+     * 自底向上
+     * https://leetcode-cn.com/problems/balanced-binary-tree/solution/balanced-binary-tree-di-gui-fang-fa-by-jin40789108/
+     */
     public boolean isBalanced(TreeNode root) {
+        return recur(root) != -1;
+    }
 
-
-        return false;
+    private int recur(TreeNode root) {
+        if (root == null) return 0;
+        int left = recur(root.left);
+        if (left == -1) return -1;
+        int right = recur(root.right);
+        if (right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
     }
 
 
-
-    class ReturnNode {
-        boolean isBalance;
-        int depth;
-
-        public ReturnNode(boolean isBalance, int depth) {
-            this.isBalance = isBalance;
-            this.depth = depth;
-        }
+    /**
+     * 从顶至底（暴力法）
+     */
+    public boolean solution1(TreeNode root) {
+        if (root == null) return true;
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && solution1(root.left) && solution1(root.right);
     }
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
 
 }
