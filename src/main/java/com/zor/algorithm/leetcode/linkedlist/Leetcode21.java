1eaa4af98cd64fa72f4665c97c7b14e4c898c10e
Sonder <kuqi007@qq.com>
2021-10-26 09:24:29 +0800

    feat: Leetcode21 两种新写法

--- src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode21.java
+++ src/main/java/com/zor/algorithm/leetcode/linkedlist/Leetcode21.java
@@ -20,9 +20,54 @@ public class Leetcode21 {
     public static void main(String[] args) {
         ListNode listNode1 = ListNodeUtil.getListNode(1, 2, 4);
         ListNode listNode2 = ListNodeUtil.getListNode(1, 3, 4);
-        ListNode listNode = solution1(listNode1, listNode2);
+        ListNode listNode = solution3(listNode1, listNode2);
         ListNodeUtil.printList(listNode);
 
+    }
+
+
+    /**
+     * 分支合并
+     */
+    public static ListNode solution4(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode head = dummyNode;
+        while (l1 != null || l2 != null) {
+            if (l1 == null || (l2 != null && l1.val > l2.val)) {
+                head.next = l2;
+                l2 = l2.next;
+            } else {
+                head.next = l1;
+                l1 = l1.next;
+            }
+            head = head.next;
+        }
+        return dummyNode.next;
+    }
+
+    /**
+     * 四个分支
+     */
+    public static ListNode solution3(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode head = dummyNode;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                head.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                head.next = l1;
+                l1 = l1.next;
+            } else if (l1.val > l2.val) {
+                head.next = l2;
+                l2 = l2.next;
+            } else {
+                head.next = l1;
+                l1 = l1.next;
+            }
+            head = head.next;
+        }
+        return dummyNode.next;
     }
 
     public static ListNode solution1(ListNode l1, ListNode l2) {
