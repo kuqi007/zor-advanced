c780c2840fc80aea108f0c09071b3d0a35556098
Sonder <kuqi007@qq.com>
2022-06-01 11:59:12 +0800

    feat: Leetcode257、Leetcode1022树的全部路径

--- src/main/java/com/zor/algorithm/leetcode/tree/Leetcode1022.java
+++ src/main/java/com/zor/algorithm/leetcode/tree/Leetcode1022.java
@@ -1,5 +1,13 @@
 package com.zor.algorithm.leetcode.tree;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 1022. 从根到叶的二进制数之和
  * 给出一棵二叉树，其上每个结点的值都是 0 或 1 。每一条从根到叶的路径都代表一个从最高有效位开始的二进制数。
@@ -29,8 +37,117 @@ package com.zor.algorithm.leetcode.tree;
  * Node.val 仅为 0 或 1
  */
 public class Leetcode1022 {
-    public int sumRootToLeaf(TreeNode root) {
+    public static void main(String[] args) {
+        Leetcode1022 leetcode257 = new Leetcode1022();
+        TreeNode root = TreeNodeUtil.constructBinaryTree(1, 0, 1, 0, 1, 0, 1);
+        int i = leetcode257.solution3(root);
+        System.out.println(i);
+    }
 
-        return 0;
+    public int solution3(TreeNode root) {
+        dfs1(root, 0);
+        return ans;
+    }
+
+
+    private int ans = 0;
+
+    private void dfs1(TreeNode root, int cur) {
+        int ncur = cur << 1 | root.val;
+        if (root.left != null) dfs1(root.left, ncur);
+        if (root.right != null) dfs1(root.right, ncur);
+        // 叶子节点，结束逻辑
+        if (root.left == null && root.right == null) {
+            ans += ncur;
+        }
+    }
+
+
+    public int solution2(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int cur) {
+        int ans = 0;
+        int ncur = cur << 1 | root.val;
+        if (root.left != null) ans += dfs(root.left, ncur);
+        if (root.right != null) ans += dfs(root.right, ncur);
+        return root.left == null && root.right == null ? ncur : ans;
+    }
+
+    /**
+     * 前序遍历迭代做法
+     */
+    public int solution1(TreeNode root) {
+        Deque<Object> stack = new LinkedList<>();
+        stack.push(root);
+        stack.push(root.val);
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            int num = (int) stack.pop();
+            TreeNode cur = (TreeNode) stack.pop();
+
+            // 叶子节点
+            if (cur.left == null && cur.right == null) {
+                sum += num;
+            }
+
+            if (cur.left != null) {
+                stack.push(cur.left);
+                stack.push(num * 2 + cur.left.val);
+            }
+
+            if (cur.right != null) {
+                stack.push(cur.right);
+                stack.push(num * 2 + cur.right.val);
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * 递归
+     */
+    public int sumRootToLeaf(TreeNode root) {
+        List<Integer> path = new ArrayList<>();
+        AtomicInteger sum = new AtomicInteger(0);
+        traversal(root, path, sum);
+        return sum.get();
+    }
+
+    private void traversal(TreeNode cur, List<Integer> path, AtomicInteger sum) {
+        path.add(cur.val);
+        if (cur.left == null && cur.right == null) {
+            int x = 0;
+            for (Integer i : path) {
+                x = x * 2 + i;
+            }
+            sum.getAndAdd(x);
+            return;
+        }
+
+        if (cur.left != null) {
+            traversal(cur.left, path, sum);
+            path.remove(path.size() - 1);
+        }
+        if (cur.right != null) {
+            traversal(cur.right, path, sum);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    private int binToDeci(String s) {
+        char[] cs = s.toCharArray();
+        int x = 0;
+        for (char c : cs) {
+            x = x * 2 + (c == '1' ? 1 : 0);
+        }
+        return x;
+    }
+
+    @Test
+    public void test() {
+        int i = binToDeci("101");
+        System.out.println(i);
     }
 }
--- src/main/java/com/zor/algorithm/leetcode/tree/Leetcode257.java
+++ src/main/java/com/zor/algorithm/leetcode/tree/Leetcode257.java
@@ -1,27 +1,128 @@
 package com.zor.algorithm.leetcode.tree;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 257. 二叉树的所有路径
  * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
- *
+ * <p>
  * 叶子节点 是指没有子节点的节点。
- *
- *
+ * <p>
+ * <p>
  * 示例 1：
- *
- *
+ * <p>
+ * <p>
  * 输入：root = [1,2,3,null,5]
  * 输出：["1->2->5","1->3"]
  * 示例 2：
- *
+ * <p>
  * 输入：root = [1]
  * 输出：["1"]
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 树中节点的数目在范围 [1, 100] 内
  * -100 <= Node.val <= 100
  */
 public class Leetcode257 {
+    public static void main(String[] args) {
+        Leetcode257 leetcode257 = new Leetcode257();
+        TreeNode root = TreeNodeUtil.constructBinaryTree(1, 2, 3, null, 5);
+        List<String> res = leetcode257.solution2(root);
+        System.out.println(res);
+    }
+
+    /**
+     * 迭代法
+     */
+    public List<String> solution2(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        Deque<Object> stack = new LinkedList<>();
+        // 节点和路径同时进栈
+        stack.push(root);
+        stack.push(root.val + "");
+        while (!stack.isEmpty()) {
+            // 节点和路径同时出战栈
+            String path = (String) stack.pop();
+            TreeNode cur = (TreeNode) stack.pop();
+            if (cur.left == null && cur.right == null) {
+                res.add(path);
+                continue;
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+                stack.push(path + "->" + cur.left.val);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+                stack.push(path + "->" + cur.right.val);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 通俗易懂解法
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        if (root == null) return res;
+        traversal(root, path, res);
+        return res;
+    }
+
+    private void traversal(TreeNode cur, List<Integer> path, List<String> res) {
+        // 添加当前节点到path
+        path.add(cur.val);
+        // 遇到叶子节点结束，即左右节点均为空的时候
+        if (cur.left == null && cur.right == null) {
+            StringBuilder sbPath = new StringBuilder();
+            for (int j = 0; j < path.size() - 1; j++) {
+                Integer i = path.get(j);
+                sbPath.append(i).append("->");
+            }
+            sbPath.append(path.get(path.size() - 1));
+            res.add(sbPath.toString());
+            return;
+        }
+
+        // 确定单层逻辑
+        if (cur.left != null) {
+            traversal(cur.left, path, res);
+            // 回溯
+            path.remove(path.size() - 1);
+        }
+        if (cur.right != null) {
+            traversal(cur.right, path, res);
+            // 回溯
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public List<String> solution1(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        String path = "";
+        if (root == null) return res;
+        travel(root, path, res);
+        return res;
+    }
+
+    private void travel(TreeNode cur, String path, List<String> res) {
+        path += cur.val;
+        if (cur.left == null && cur.right == null) {
+            res.add(path);
+            return;
+        }
+        if (cur.left != null) {
+            travel(cur.left, path + "->", res);
+        }
+        if (cur.right != null) {
+            travel(cur.right, path + "->", res);
+        }
+    }
 }
